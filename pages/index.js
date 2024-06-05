f563d2f3eb257f82174ee8383714115e81597650
Lucas Ferreira <69616909+FSLucas22@users.noreply.github.com>
2024-06-05 21:26:37 +0000

    Primeiro componente

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,42 @@
+import { useState } from "react";
+
 function Home() {
-    return <h1>Carol, te amo tanto que uma orelha não seria presente o bastante.
-        Pode ficar comigo por inteiro! rsrs</h1>
+    return <QuizzLine 
+        question = {"Te amo tanto que uma _________ não "
+                + "seria presente o bastante. Pode ficar comigo "
+                + "por inteiro! rsrs"}
+        placeholder = {"completa aí rs"}
+        verifyAnswer = {(answer) => answer.toLowerCase() === "orelha"}>
+    </QuizzLine>
+}
+
+function QuizzLine(props) {
+    const {question, placeholder, verifyAnswer} = props
+    
+    const [answer, setAnswer] = useState("")
+
+    const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
+
+    if (isAnswerCorrect) {
+        return <p>Acertoooou!</p>
+    }
+
+    return <div>
+        <p>{ question }</p>
+        <input
+            type="text"
+            name="nome"
+            autoComplete="off"
+            placeholder={placeholder}
+            className="col-12"
+            value={answer}
+            onChange={(evento) => setAnswer(evento.target.value)}
+          />
+        
+        <button 
+            onClick={() => setIsAnswerCorrect(verifyAnswer(answer))}>
+        Acho que acertei!</button>
+    </div>
 }
 
 export default Home;
