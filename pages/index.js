c3c2506c335b8c17c78fb69a30e7ed646f1e8e1d
Lucas Ferreira <69616909+FSLucas22@users.noreply.github.com>
2024-06-05 15:30:16 +0000

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
     return <h1>Carol, te amo tanto que uma orelha não seria presente o bastante.
-        Pode ficar comigo por inteiro! rsrs</h1>
+        Pode ficar comigo por inteiro!!!</h1>
 }
 
 export default Home;
