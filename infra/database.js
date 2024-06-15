6e4efce7220428e6aebd7ce8b0447c8b9b5daf5c
Lucas Ferreira <69616909+FSLucas22@users.noreply.github.com>
2024-06-15 02:51:17 +0000

    remove logs

--- infra/database.js
+++ infra/database.js
@@ -10,15 +10,6 @@ function clientFactory() {
     ssl: process.env.NODE_ENV !== "development",
   });
 
-  console.log("Credenciais do Postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV !== "development",
-  });
-
   return client;
 }
 
