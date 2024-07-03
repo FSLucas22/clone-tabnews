bdd893a2a540cd2dbac8f31ebf422e02270f1cbb
Lucas Ferreira <69616909+FSLucas22@users.noreply.github.com>
2024-07-03 19:38:53 +0000

    improve orchestrator

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -2,16 +2,26 @@ import retry from "async-retry";
 
 async function waitForAllServices() {
   await waitForWebServer();
-
   async function waitForWebServer() {
+    const retries = 100;
+    const endpoint = "api/v1/status";
     return retry(fetchStatusPage, {
-      retries: 100,
+      retries,
       maxTimeout: 1000,
+      onRetry: (error, attempt) => {
+        if (attempt % 5 == 0) {
+          console.log(
+            `Attempt ${attempt}/${retries} - Failed to fetch ${endpoint} endpoint: ${error.message}`,
+          );
+        }
+      },
     });
 
     async function fetchStatusPage() {
-      const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
+      const response = await fetch(`http://localhost:3000/${endpoint}`);
+      if (response.status !== 200) {
+        throw Error(`Endpoint returned with status code ${response.status}`);
+      }
     }
   }
 }
