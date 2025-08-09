3e8d72d4636d20691fc6ad91c2d1998b9cc921a4
Lucas Ferreira <69616909+FSLucas22@users.noreply.github.com>
2025-08-09 06:51:11 +0000

    feat: add endpoint `POST api/v1/users`

--- infra/controller.js
+++ infra/controller.js
@@ -1,4 +1,8 @@
-import { MethodNotAllowedError, InternalServerError } from "./errors";
+import {
+  MethodNotAllowedError,
+  InternalServerError,
+  ValidationError,
+} from "./errors";
 
 function onNoMatchHandler(request, response) {
   const publicErrorObject = new MethodNotAllowedError();
@@ -6,6 +10,10 @@ function onNoMatchHandler(request, response) {
 }
 
 function onErrorHandler(error, request, response) {
+  if (error instanceof ValidationError) {
+    return response.status(error.statusCode).json(error);
+  }
+
   const publicErrorObject = new InternalServerError({
     cause: error,
     statusCode: error.statusCode,
---
+++ pages/api/v1/users/index.js
@@ -0,0 +1,14 @@
+import { createRouter } from "next-connect";
+import controller from "infra/controller";
+import user from "models/user";
+
+const router = createRouter();
+router.post(postHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function postHandler(request, response) {
+  const userInputValues = request.body;
+  const newUser = await user.create(userInputValues);
+  return response.status(201).json(newUser);
+}
